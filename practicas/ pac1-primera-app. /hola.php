5be7597975d50b4a216c9da827cb0e19a458e7ff
daw2-blanco24 <120489729+davidblancofpllefia@users.noreply.github.com>
2024-09-12 17:33:43 +0000

    Pràctica 1. Primera aplicació en PHP - Falta mi foto

---
+++ practicas/ pac1-primera-app. /hola.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>Hola</title>
+	</head>
+	<body>
+        <header>
+            <img src="practicas/img/logo40web-.png" alt="">
+            <h1>Módulo 7 - Práctica 1. Mi primera aplicación en PHP</h1>
+        </header>
+        <img src="" alt="">
+        <p>
+            ?php: Sirve para abrir el codigo PHP y dentro poner toda la programacion en lengauje PHP
+            function sayHello($name): Crea una function llamada sayHello de la cual coge una variable llamada name
+            echo "Hello $name!": El echo basicamente hace que se vea en la pagina lo que escribe dentro del echo, en este caso pondra Hello y el contenido de la variable name
+            sayHello('remote world'): Usamos la funcion de sayHello para que diga el contenido que hay dentro del parentesis que es basicamente la variable name
+            phpinfo(): Crea un documento donde puedes ver informacion sobre PHP que pueda ser de utilidad
+        </p>
+        <footer>
+            <p>David Blanco</p>
+            <p>2024-12-09</p>
+        </footer>
+	</body>
+</html>
